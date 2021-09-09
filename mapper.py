16ddcd7717010a6fb36d8835aa06b232aaf4f0cf
RyanSchraeder <rschraeder2@gmail.com>
2021-09-08 21:06:40 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python
 import sys
-import string
-import re
-import nltk 
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 from nltk.corpus import stopwords
-
 stops = set(stopwords.words('english'))
 
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace and make words lowercase
+
+# remove leading and trailing whitespace
     line = line.strip().lower()
 
-    # remove punctuation
-    letters_only = re.sub("[^a-zA-Z", " ", line)
-     
-    # split the line into words; splits on any whitespace
-    words = letters_only.split()
+# split the line into words; splits on any whitespace
+    words = line.split().lower()
 
-    # organize the sentence with lemmatization
+    # tokenize sentences & words for frequency distribution
+    sentences = nltk.sent_tokenize(line)
+    print("Total Sentences: ", len(sentences))
 
-    for word in words: 
-        word = lemmatizer.stem(word)
-    
-    # output tuples (word, 1) of words without any stopwords in tab-delimited format
+    words = nltk.word_tokenize(words)
+    print("Total Words: ", len(words))
 
-        print '%s\t%s' % (word, "1")
+    unique_words = set(words)
+    print("Total unique words: ", len(unique_words))
 
+    final_words = []
+    # output tuples (word, 1) in tab-delimited format
+    for word in words:
+        if word not in stopwords:
+        final_words.append(word)
+        
+        print(" --- Top Words After Removing Stopwords --- ")
+        print('%s\t%s' % (word, "1"))
+        print("Total Words After Stopword Removal: ", len(final_words))
+        print("Top Word by Frequency: ", final_words.value_counts().nlargest(1))
+        
