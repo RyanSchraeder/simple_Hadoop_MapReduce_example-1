856349c7547a3ee823b646381e768e51274a29f7
RyanSchraeder <rschraeder2@gmail.com>
2021-09-08 20:43:52 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import sys
 import string
-import re 
+import re
 import nltk 
-nltk.download('stopwords')
 
 from nltk.corpus import stopwords
-word_tokens = word_tokenize()
-import nltk.sentiment.sentiment_analyzer 
 
 stops = set(stopwords.words('english'))
 
@@ -17,27 +14,17 @@ for line in sys.stdin:
     line = line.strip().lower()
 
     # remove punctuation
-    letters_only = re.sub("[^a-zA-Z", " ", raw_text)
+    letters_only = re.sub("[^a-zA-Z", " ", line)
      
     # split the line into words; splits on any whitespace
-    words = line.split()
-    
-    clean_words = []
+    words = letters_only.split()
+
+    # organize the sentence with lemmatization
 
     for word in words: 
-        if word not in stop_words: 
-            clean_words.append(word)
-
-    stemmed_words = []
-    # organize the sentence with lemmatization
-    word_tokens = word_tokenize(line)
-
-    for word in clean_words: 
-        if word in word_tokens: 
-           word = lemmatizer.stem(word)
-            stemmed_words.append(word)
+        word = lemmatizer.stem(word)
     
     # output tuples (word, 1) of words without any stopwords in tab-delimited format
-    for word in stemmed_words:
+
         print '%s\t%s' % (word, "1")
 
