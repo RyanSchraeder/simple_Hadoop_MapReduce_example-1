9ae5de22df41495f36362a890bc0db259007a35c
RyanSchraeder <rschraeder2@gmail.com>
2021-09-08 20:27:16 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env python
 import sys
-import string 
+import string
+import re 
 import nltk 
 nltk.download('stopwords')
+
 from nltk.corpus import stopwords
+word_tokens = word_tokenize()
 import nltk.sentiment.sentiment_analyzer 
 
 stops = set(stopwords.words('english'))
-positive_words = ['good', 'happy', 'excited', 'amazing', 'relieved']
-    
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace and make words lowercase
     line = line.strip().lower()
-  
+
+    # remove punctuation
+    letters_only = re.sub("[^a-zA-Z", " ", raw_text)
+     
     # split the line into words; splits on any whitespace
     words = line.split()
+    
+    clean_words = []
 
-    # analyze for positive sentiment 
-    analysis = nltk.sentiment.util.extract_unigram_feats(words, positive_words)
+    for word in words: 
+        if word not in stop_words: 
+            clean_words.append(word)
 
+    stemmed_words = []
+    # organize the sentence with lemmatization
+    word_tokens = word_tokenize(line)
+
+    for word in clean_words: 
+        if word in word_tokens: 
+           word = lemmatizer.stem(word)
+            stemmed_words.append(word)
+    
     # output tuples (word, 1) of words without any stopwords in tab-delimited format
-    for word in words:
-        if word not in stops:
+    for word in stemmed_words:
         print '%s\t%s' % (word, "1")
-        print('\n** Positive Sentiment For Each Word **\n')
-        print(analysis)
 
